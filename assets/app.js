f5d0a645c314ed55e098c2a4a5517ab3b387c53b
iryna <musicira@gmail.com>
2023-09-26 15:18:48 +0200

    doc.js update

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,29 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+
 console.log('app.js works');
+
+// const moment = require ('moment');
+// const now = moment ();
+// console.log (now.format ());
+
+
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
 
 // import jquery from 'jquery';
 // import $ from 'jquery';
-// // const $ = require('jquery');
-// global.$ = global.jQuery = $;
+// const $ = require('jquery');
+global.$ = global.jQuery = $;
+// const datepicker = require('js-datepicker')
+ 
+//import { Tooltip, Toast, Popover } from 'bootstrap';
+  
+// start the Stimulus application
+// import './bootstrap';
